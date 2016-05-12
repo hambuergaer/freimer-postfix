db3853626e3fd59256db80a8867e277c9c0dac1f
Frank Reimer <frank.reimer@kuehne-nagel.com>
2016-05-12 09:39:59 +0200

    exec systemctl daemon-reload added

--- manifests/pidfile.pp
+++ manifests/pidfile.pp
@@ -1,9 +1,16 @@
 class postfix::pidfile inherits postfix {
 
-  file_line { 'postfix.service':
+   file_line { 'postfix.service':
         path    => '/usr/lib/systemd/system/postfix.service',
         line    => "PIDFile=${postfix_queue_directory}/pid/master.pid",
         match   => '[#P]IDFile=',
         ensure  => present,
         }
+
+   exec { 'systemctl_daemon_reload':
+	command => '/bin/systemctl daemon-reload',
+	subscribe => File_line['postfix.service'],
+	refreshonly => true
+	}
+	
 }
